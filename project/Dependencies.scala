b10bf36d7ccaca78434764a0bf58bf5d8df391f0
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-03-22 20:56:27 +0100

    Update testcontainers-scala to 0.40.14 (#240)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val zio = "2.0.2"
-    val testcontainers = "0.40.12"
+    val testcontainers = "0.40.14"
   }
 
   val zio = "dev.zio" %% "zio" % Versions.zio
